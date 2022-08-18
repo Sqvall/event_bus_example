457944d81a22d8c1bec6f3478276c36d6fdc8bb8
Sqvall <masster1987@inbox.ru>
2022-08-18 11:24:25 +0300

    Initial

--- app/domain/orders/handlers.py
+++ app/domain/orders/handlers.py
@@ -3,10 +3,11 @@ from app.services.events.base import EventHandler
 
 
 class SendMessage(EventHandler):
+    # Хотя может класс тут излишен, можно функциями обойтись.
     def run(self, db, event: ChangeStatus):
         """Генерация сообщения исходя из статуса и вызов Celery таски на отправку сообщений."""
 
 
 PRODUCT_EVENTS_HANDLERS = {
-    ChangeStatus: [SendMessage]
+    ChangeStatus: [SendMessage()]
 }
